ce32584eddcfcf6771a1563cf717da90d2906bc8
camel <sultanscreed@gmail.com>
2021-01-06 19:50:47 +0600

    my first practical work

---
+++ js/practice.js
@@ -0,0 +1,6 @@
+"use strict";
+// Practical work №1
+let numberOfFilms;
+numberOfFilms = prompt("How many films have you watched so far?", "");
+console.log(`You have already watched ${numberOfFilms} flims!`);
+
