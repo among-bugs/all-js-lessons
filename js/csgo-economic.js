000100b5dde6c4f366794bd1f23a635938f6626c
camel <sultanscreed@gmail.com>
2021-01-09 04:09:55 +0600

    modificate code for csgo-economic

--- js/csgo-economic.js
+++ js/csgo-economic.js
@@ -1,10 +1,5 @@
 "use strict";
 
-let pistolsPrices = [],
-    over100PistolsLists = [],
-    modifiedWeaponsDetails = [],
-    AllmodifiedWeapons = [];
-
 const examples = [
     "pistols",
     "shotguns",
@@ -14,18 +9,31 @@ const examples = [
     "machineGuns",
     "grenades"
 ];
+
 let InModifiedWeaponsDetails = [];
 for (let indx = 0; indx < weapons.length; indx++) {
 
     let Info = [];
 
     for (let indInn = 0; indInn < weapons[indx].length; indInn++) {
-        Info.push(`weapon: ${weapons[indx][indInn].name} >>> price:${
-            weapons[indx][indInn].Price[0] + currency.USD}`);
+        // Info.push(`weapon: ${weapons[indx][indInn].name} >>> price:${
+        //     weapons[indx][indInn].Price[0] + currency.USD}`);
+        let usingInd = [], curr = currency.USD;
+        usingInd = weapons[indx][indInn];
+        Info.push({
+            name: usingInd.name,
+            price: usingInd.Price[0] +
+                currency.USD,
+            magazineCapacity: usingInd.MagazineCapacity,
+            killReward: {
+                competitiveMode: usingInd.KillReward.CompetitiveMode[0] + curr,
+                normalMode: weapons[indx][indInn].KillReward.NormalMode[0] + curr
+            },
+            damage: usingInd.Damage
+        });
     }
-
     InModifiedWeaponsDetails.push({
         category: examples[indx],
         information: Info
     });
-};
+}
