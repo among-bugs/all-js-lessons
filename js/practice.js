5d974ac97458cbb45496c0e83df1ee699922957e
camel <sultanscreed@gmail.com>
2021-01-13 22:43:16 +0600

    second practical work is done

--- js/practice.js
+++ js/practice.js
@@ -24,20 +24,69 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
 
 let numberOfFilms;
-numberOfFilms = prompt("How many films have you watched so far?", "");
+numberOfFilms = +prompt("How many films have you watched so far?", "");
 console.log(`You have already watched ${numberOfFilms} flims!`);
 
-let lastWatchedFilms = prompt("Your last watched film?", "8 mile");
-let score = prompt("Your score for this flim?", "9.5");
+let questions = ["Your last watched film?", "Your score for this flim?"];
+
+let answers = [];
+let lastWatchedFilms, score;
 
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: {
-        lastWatchedFilms: score
-    },
-    actors: "",
-    genres: "",
+    movies: {},
+    actors: {},
+    genres: [],
     private: false
 };
+
+let defaultAnswers = {
+    films: ['green mile', 'eminem', 'god', 'smells like', 'revolver', 
+            'knifeshot', 'jungle', 'qwerty', 'mr robot', 'mrx'],
+    scores: [9.5, 8.0, 9.6, 9.4, 6.5, 4.7, 8.8, 9.9, 7.8, 8.9]
+};
+
+for (let indx = 0; indx < 10; indx++){
+    let urLastWatchedFilm = prompt(questions[0], defaultAnswers.films[indx]);
+    let urScoreForThisFilm = prompt(questions[1], defaultAnswers.scores[indx]);
+
+    if (urLastWatchedFilm != null && urScoreForThisFilm != null && 
+        urScoreForThisFilm != '' && urScoreForThisFilm != '') {
+            personalMovieDB.movies[urLastWatchedFilm] = urScoreForThisFilm; 
+    } else {
+        alert(`Your answer is empty. Please, try again`);
+        indx--;
+    }
+    alert(`film: ${urLastWatchedFilm} score: ${urScoreForThisFilm}`);
+}
+
+if (personalMovieDB.count < 10){
+    console.log(`your watched films are less than 10 :()`);
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+    console.log(`your watched films are between 10 and 30, you r cool!`);
+} else if (personalMovieDB.count >= 30) {
+    console.log(`OMG! U r god!`);
+} else {
+    console.log(`unknown!`);
+}
+
+
+
+
+
