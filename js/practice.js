200b46e2baf88ce762c1068e50f2d3b90682f748
camel <sultanscreed@gmail.com>
2021-01-17 14:52:54 +0600

    18-th pratical work completed!

--- js/practice.js
+++ js/practice.js
@@ -40,8 +40,22 @@
 
 
 let numberOfFilms;
-numberOfFilms = +prompt("How many films have you watched so far?", "");
-console.log(`You have already watched ${numberOfFilms} flims!`);
+// numberOfFilms = +prompt("How many films have you watched so far?", "");
+// console.log(`You have already watched ${numberOfFilms} flims!`);
+
+let ObjQuestions = {
+    first: `How many films have you watched so far?`
+};
+
+function start() {
+    numberOfFilms = +prompt(ObjQuestions.first, '');
+
+    while ( numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt(ObjQuestions.first, '');
+    }
+}
+
+start();
 
 let questions = ["Your last watched film?", "Your score for this flim?"];
 
@@ -89,17 +103,74 @@ for (let indx = 0; indx < 10; indx++){
     });
 }
 
-if (personalMovieDB.count < 10){
-    console.log(`your watched films are less than 10 :()`);
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
-    console.log(`your watched films are between 10 and 30, you r cool!`);
-} else if (personalMovieDB.count >= 30) {
-    console.log(`OMG! U r god!`);
-} else {
-    console.log(`unknown!`);
+function rememberMyFilms() {
+
+    for (let indx = 0; indx < 10; indx++){
+        let urLastWatchedFilm = prompt(questions[0], defaultAnswers.films[indx]);
+        let urScoreForThisFilm = prompt(questions[1], defaultAnswers.scores[indx]);
+    
+        if (urLastWatchedFilm != null && urScoreForThisFilm != null && 
+            urScoreForThisFilm != '' && urScoreForThisFilm != '') {
+                personalMovieDB.movies[urLastWatchedFilm] = urScoreForThisFilm; 
+        } else {
+            alert(`Your answer is empty. Please, try again`);
+            indx--;
+        }
+          
+        let currentFilmsInformation = 
+        `${indexes[indx]} film: ${urLastWatchedFilm} score: ${urScoreForThisFilm}`;
+        alert(currentFilmsInformation);
+        moviesInformation.push({
+            id: indexes[indx],
+            film: urLastWatchedFilm,
+            score: urScoreForThisFilm
+        });
+    }
+}
+function detectLevel() {
+
+    if (personalMovieDB.count < 10){
+        console.log(`your watched films are less than 10 :()`);
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+        console.log(`your watched films are between 10 and 30, you r cool!`);
+    } else if (personalMovieDB.count >= 30) {
+        console.log(`OMG! U r god!`);
+    } else {
+        console.log(`unknown!`);
+    }
 }
 
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+function showMyDB(isHidden) {
+    if (!isHidden) {
+        console.log(personalMovieDB.movies);
+    } else {
+        console.log(personalMovieDB.movies);
+    }
+}
+
+console.log(showMyDB(personalMovieDB.private));
 
 
 
+function writeYourGenres() {
+    const exapmleGenres = ['rock', 'blues', 'jazz'];
+    for (let ind = 0; ind < 3; ind++) {
+        personalMovieDB.genres[ind] = prompt(`Ваш любимый жанр под номером ${ind + 1}`,
+                                         exapmleGenres[ind]);
+    }
+}
 
+writeYourGenres();
